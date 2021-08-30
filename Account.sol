47774cbeb1c5891a5516234c9760240e7058f4df
baerelektro <baer.elektro@web.de>
2021-08-30 16:41:36 +0200

    Update Projekt

--- Account.sol
+++ Account.sol
@@ -1,46 +1,45 @@
-pragma ton-solidity ^0.39.0;
+pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
-pragma AbiHeader pubkey;
 
+/// @title Simple wallet
+/// @author Tonlabs
+contract Wallet {
+    /*
+     Exception codes:
+      100 - message sender is not a wallet owner.
+      101 - invalid transfer value.
+     */
 
-import "./../access/ExternalOwner.sol";
-import "./../utils/RandomNonce.sol";
-import "./../utils/CheckPubKey.sol";
-
-
-/*
-    @title Simple externally owned contract
-    @dev Allows to interact with any smart contract by sending an internal messages
-    or simply transfer TONs
-*/
-contract Account is ExternalOwner, RandomNonce, CheckPubKey {
-    constructor() public checkPubKey {
+    /// @dev Contract constructor.
+    constructor() public {
+        // check that contract's public key is set
+        require(tvm.pubkey() != 0, 101);
+        // Check that message has signature (msg.pubkey() is not zero) and message is signed with the owner's private key
+        require(msg.pubkey() == tvm.pubkey(), 102);
         tvm.accept();
-
-        setOwnership(msg.pubkey());
     }
 
-    /*
-        @notice Send transaction to another contract
-        @param dest Destination address
-        @param value Amount of attached TONs
-        @param bounce Message bounce
-        @param flags Message flags
-        @param payload Tvm cell encoded payload, such as method call
-    */
-    function sendTransaction(
-        address dest,
-        uint128 value,
-        bool bounce,
-        uint8 flags,
-        TvmCell payload
-    )
-        public
-        view
-        onlyOwner
-    {
-        tvm.accept();
 
-        dest.transfer(value, bounce, flags, payload);
+    // Modifier that allows function to accept external call only if it was signed
+    // with contract owner's public key.
+    modifier checkOwnerAndAccept {
+        // Check that inbound message was signed with owner's public key.
+        // Runtime function that obtains sender's public key.
+        require(msg.pubkey() == tvm.pubkey(), 100);
+
+		// Runtime function that allows contract to process inbound messages spending
+		// its own resources (it's necessary if contract should process all inbound messages,
+		// not only those that carry value with them).
+		tvm.accept();
+		_;
+	}
+
+    /// @dev Allows to transfer tons to the destination account.
+    /// @param dest Transfer target address.
+    /// @param value Nanotons value to transfer.
+    /// @param bounce Flag that enables bounce message in case of target contract error.
+    function sendTransaction(address dest, uint128 value, bool bounce) public pure checkOwnerAndAccept {
+         // Runtime function that allows to make a transfer with arbitrary settings.
+        dest.transfer(value, bounce, 0);
     }
 }
