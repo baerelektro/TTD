058b9f68904c2391266dde00296e280ed9acb652
baerelektro <baer.elektro@web.de>
2021-09-06 11:22:43 +0200

    Zombies

--- Zombies/Contract.sol
+++ Zombies/Contract.sol
@@ -1,8 +1,9 @@
+// pragma solidity ^0.4.19;
 pragma ton-solidity >= 0.35.0;
 
 contract ZombieFactory {
 
-    // Здесь объяви событие
+    event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -14,17 +15,22 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
-        // Здесь запусти событие
+    //function _createZombie(string _name, uint _dna) private {
+    function _createZombie(string _name, uint _dna) private view {    
+        //uint id = uint(zombies.push(Zombie(_name, _dna)) - 1);
+        uint id = zombies.length;
+        //NewZombie(id, _name, _dna);
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string _str) private view returns (uint) {
+        //uint rand = uint(keccak256(_str));
         uint rand = uint(sha256(_str));
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string _name) public {
+    //function createRandomZombie(string _name) public {
+    function createRandomZombie(string _name) public view {
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
